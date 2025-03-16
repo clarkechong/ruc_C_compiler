067d460b399a9fc7a61d719dfabe443c6d328f67
Clarke Chong <149320067+clarkechong@users.noreply.github.com>
2025-03-16 23:56:35 +0000

    modify toolchain_test.sh to be independent of the compiler

--- scripts/toolchain_test.sh
+++ scripts/toolchain_test.sh
@@ -5,10 +5,12 @@
 
 set -euo pipefail
 
-make bin/c_compiler
-rm -f bin/riscv_example.s
-./bin/c_compiler -S "compiler_tests/_example/example.c" -o "bin/riscv_example.s"
-riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "bin/riscv_example" "bin/riscv_example.s" "compiler_tests/_example/example_driver.c"
+# make bin/c_compiler
+# rm -f bin/riscv_example.s
+# ./bin/c_compiler -S "compiler_tests/_example/example.c" -o "bin/riscv_example.s"
+
+# riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "bin/riscv_example" "bin/riscv_example.s" "compiler_tests/_example/example_driver.c"
+riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "bin/riscv_example" "text/toolchain.s" "text/toolchain.c"
 
 set +e
 spike pk "bin/riscv_example"
