99eade049a34c1ecad592eea15b360e36a12744f
Clarke Chong <149320067+clarkechong@users.noreply.github.com>
2025-03-17 11:55:10 +0000

    do primary_expression

--- parser.sh
+++ parser.sh
@@ -1,2 +1,2 @@
 make all
-./bin/parser_main ./compiler_tests/_example/example.c
+./bin/parser_main ./text/sometext.c
--- text/sometext.c
+++ text/sometext.c
@@ -2,6 +2,6 @@ int main() {
     return 25.5;    // float
     return 10;      // integer
     return "hello"; // string
-    // return `c`;     // char
-    // return hello;   // identifier
+    return 'c';     // char
+    return hello;   // identifier
 }
